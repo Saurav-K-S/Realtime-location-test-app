3478764464a4b6bcca4fb9e9c3d6b136c43fde93
Saurav-K-S <67679062+Saurav-K-S@users.noreply.github.com>
2023-05-21 18:37:23 +0530

    App created

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Home());
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+      ),
+    );
+  }
+}
